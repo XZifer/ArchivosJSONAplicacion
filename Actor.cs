f94e234b61bc0f35ce724253108158317d2de350
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-28 14:47:30 -0500

    Creando Clase Actor  y sus propiedades

---
+++ Actor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivosJSONAplicacion
+{
+    public class Actor
+    {
+        public string nombre { get; set; }
+        public DateTime fechaDeNacimiento  { get; set; }
+    }
+}
