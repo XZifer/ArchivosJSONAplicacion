ab4e3ffd64da5042c2197b05446772a99d10ace2
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-28 14:42:41 -0500

    Generando enum TipoGenero

---
+++ TipoGenero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivosJSONAplicacion
+{
+    enum TipoGenero
+    {
+        Accion,
+        Suspenso,
+        Romance,
+        Cienciaficcion,
+        Drama,
+        Fantasia
+    }
+}
