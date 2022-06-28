2a868dc1908c9e50c1851553969f87642c6f4063
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-28 16:03:13 -0500

    Clase Pelicula y sus propiedades

---
+++ Pelicula.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivosJSONAplicacion
+{
+    class Pelicula
+    {
+        public string Nombre { get; set; }
+        public int Año { get; set; }
+        public TipoGenero Genero { get; set; }
+        public bool EsParaAdultos { get; set; }
+        public List<Actor> Actores { get; set; }
+    }
+}
